9c9b067e84dd3347b175a83ae4ba3f1c2c3ababf
Logan <55439289+afrench14@users.noreply.github.com>
2020-12-17 14:28:51 +0000

    aAAAAAAa

--- non repeated random names.py
+++ non repeated random names.py
@@ -4,12 +4,24 @@ list_a = ["Mike","Bob","June"]
 list_b = ["Red","Green"]
 list_c = ["Run","Jump"]
 
-list_so_far = []
-
 def user_generator():
-    part_a = random.choice(list_a)
-    part_b = random.choice(list_b)
-    part_c = random.choice(list_c)
+    
+    amount_of_names = int(input("how many names do you need?: "))
 
-    temporary_name = (part_a, part_b, part_c)
-    list_so_far.append(temporary_name)
+    list_so_far = []
+    final_list = []
+
+    for in amount_of_names:
+        part_a = random.choice(list_a)
+        part_b = random.choice(list_b)
+        part_c = random.choice(list_c)
+        temporary_name = (part_a, part_b, part_c)
+        list_so_far.append(temporary_name)
+
+    for i in list_so_far:
+        if i not in final_list:
+            final_list.append(i)
+    
+    print(final_list)
+
+
